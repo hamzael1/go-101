143ed8828ef8663d3b1b99890b0e4b4276830c9f
Hamza El <hamza.elbouatmani@gmail.com>
2020-04-29 22:49:42 +0000

    Add Package-Import

---
+++ 10-Package-Import/app/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"go-101/10-Package-Import/mypkg"
+)
+
+func main() {
+	fmt.Println(mypkg.PubVar)
+}
---
+++ 10-Package-Import/mypkg/file.go
@@ -0,0 +1,4 @@
+package mypkg
+
+var PubVar string = "Hello ! I am inside the package mypkg"
+var hidden string = "This can't be accessed outside the package"
